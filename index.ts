00f4b68874e888a63816bd8f75768fe874d82c12
AlizyKhi <162714208+AlizyKhi@users.noreply.github.com>
2024-04-26 19:29:04 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,64 @@
+let fruitName = "mango";
+console.log(fruitName)
+function logfruitName(){
+let fruitName = "PineApple";
+console.log (fruitName)    
+}
+logfruitName()
+
+let carName = "Toyota";
+console.log (carName)
+function logcarName(){
+let carName = "Mehran";
+console.log (carName)    
+}
+logcarName()
+
+let subjectName = "Math's";
+console.log (subjectName)
+function logsubjectName(){
+}
+logsubjectName()
+
+let seasonType = "Winter";
+console.log (seasonType)
+function logseasonType(){
+let seasonType = "Autumn";
+console.log (seasonType)     
+}
+logseasonType()
+
+let dressType = "Umbrella";
+console.log (dressType)
+function logdressType(){    
+}
+logdressType()
+
+
+function logFruitName(fruitName,carName){
+console.log (carName,fruitName)
+}                                 
+logFruitName("civics","grande")
+
+function logMakeupCollection(Lipstick,Powder,Eyeliner,Shadow,Lenses){
+console.log(Lipstick,Powder,Eyeliner,Shadow,Lenses)    
+}
+logMakeupCollection("Lipstick","Powder","Eyeliner","Shadow","Lenses")
+
+function logGirlsName(Ambreen,Nasreen,Zahida,Shabana,Farzana){
+console.log (Ambreen,Nasreen,Zahida,Shabana,Farzana)
+}
+logGirlsName("Farzana","Sultana","Ruqqaiay","Nimra","Shahmeer")   
+
+function logBoysName(zahid,anwar,baloch,pathan,punjabi){
+console.log (zahid,anwar,baloch,pathan,punjabi)    
+}
+logBoysName("armagaan","shaheer","basit","sheryaar","muzammil")
+
+
+
+
+
+
+
+
